ccffabd1ddbac25115f0817119e14c0a3a9ccd3d
Litun098 <nayaklitun9@gmail.com>
2022-12-16 10:25:35 +0530

    complete

--- index.js
+++ index.js
@@ -6,27 +6,28 @@ const io = require('socket.io')(server,{
         origin:"*",
         methods:["GET","POST"]
     }
-})
+});
+require('dotenv').config()
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.get('/',(req,res)=>{
-    res.send("Server is running.");
+    res.send("Running.");
 })
 
 io.on('connection',(socket)=>{
     socket.emit('me',socket.id);
 
-    socket.on('dicconnect',()=>{
-        socket.broadcast.emit("callended");
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit("callEnded");
     })
 
-    socket.on('calluser',({userToCall,signalData,from,name})=>{
-        io.to(userToCall).emit('calluser',{signal:signalData,from,name});
+    socket.on('callUser',({userToCall,signalData,from,name})=>{
+        io.to(userToCall).emit('callUser',{signal:signalData,from,name});
     })
     socket.on('answerCall',(data)=>{
-        io.to(data.to).emit('callaccepted',data.signal);
+        io.to(data.to).emit('callAccepted',data.signal);
     })
 })
 server.listen(PORT,()=>{
